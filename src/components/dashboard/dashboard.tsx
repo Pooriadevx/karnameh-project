e784bc767c10fa24bcb20e5651f5180b058bb968
Pooriadevx <pooria.b1378@gmail.com>
2024-08-11 16:23:20 +0330

    fix: Added searchbar to handle filter list by name and email

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,16 +1,34 @@
-import React, { useContext } from "react";
-import { Grid } from "@mui/material";
+import React, { useContext, useState } from "react";
+import { Grid, TextField } from "@mui/material";
 import { DashboardCtx } from "../../context/dashboard";
 import Table from "../table/table";
 import { DashboardCtxType } from "../../types/dashboard";
 import Modal from "../modal";
 
 const Dashboard: React.FC = () => {
+  const [value, setValue] = useState<string>("");
   const { modalData, list } = useContext(DashboardCtx) as DashboardCtxType;
+
+  const currentList = !!list
+    ? list.filter(
+        (user) =>
+          user.name.toLowerCase().includes(value.toLowerCase()) ||
+          user.email.toLowerCase().includes(value.toLowerCase())
+      )
+    : [];
 
   return (
     <Grid container direction="column" minHeight={480}>
-      <Table list={list || []} />
+      <Grid item margin={2}>
+        <TextField
+          placeholder="search..."
+          value={value}
+          size="small"
+          spellCheck
+          onChange={({ target }) => setValue(target.value)}
+        />
+      </Grid>
+      <Table list={currentList} />
       {modalData && <Modal />}
     </Grid>
   );
